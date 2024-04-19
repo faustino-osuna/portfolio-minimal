bf28335a4785c5762e9496a998530a878b5053a9
Faustino Osuna <144746003+faustino-osuna@users.noreply.github.com>
2024-04-19 16:58:32 -0700

    modify readme.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://faustino-osuna.github.io',
+  base: 'portfolio-minimal',
+});
